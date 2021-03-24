3dcd3dab20bec50e79029ba7fc2ba6983c29d77f
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2021-03-24 10:27:34 +0100

    gauge metrics

--- src/main/java/mw/metrics/MetricsConfiguration.java
+++ src/main/java/mw/metrics/MetricsConfiguration.java
@@ -3,14 +3,17 @@ package mw.metrics;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 public class MetricsConfiguration {
 
     @Bean
@@ -19,16 +22,20 @@ public class MetricsConfiguration {
     }
 
     @Configuration
-    public static class MetricsGraphiteConfiguration{
+    public static class MetricsGraphiteConfiguration {
+
         @Autowired
         MetricRegistry metricRegistry;
 
         @PostConstruct
-        public void startGraphiteReporter() throws UnknownHostException {
-
-            Graphite graphite=new Graphite("192.168.0.113",2003);
-            var reporter = GraphiteReporter.forRegistry(metricRegistry).prefixedWith("services.movie").build(graphite);
-            reporter.start(10, TimeUnit.SECONDS);
+        public void startGraphiteReporter()
+            throws UnknownHostException {
+            var host = InetAddress.getLocalHost().getHostAddress();
+            Graphite graphite = new Graphite("192.168.0.113", 2003);
+            var reporter = GraphiteReporter.forRegistry(metricRegistry)
+                                           .prefixedWith(String.format("services.movie %s", host))
+                                           .build(graphite);
+            reporter.start(3, TimeUnit.SECONDS);
         }
 
     }
--- src/main/java/mw/metrics/MovieService.java
+++ src/main/java/mw/metrics/MovieService.java
@@ -1,16 +1,43 @@
 package mw.metrics;
 
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieService {
 
+    @Autowired
+    private MetricRegistry metricRegistry;
+    private Map<String, String> cache = new HashMap<>();
+
+    @PostConstruct
+    public void registerMetric() {
+        metricRegistry.register("CACHE_SIZE", (Gauge<Integer>) () -> cache.size());
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    public void scheduleFixedDelayTask() {
+        System.out.println("Fixed delay task - " + System.currentTimeMillis() / 1000);
+        getFor("");
+    }
+
     public String getFor(String name) {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
+
+       // try {
+
+            cache.put(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+
+
+     /*   } catch (InterruptedException e) {
             throw new IllegalArgumentException(e);
-        }
+        }*/
         return "Test";
     }
 
