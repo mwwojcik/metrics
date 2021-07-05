899621883104ee2e8045e7a79ac57f11bc7a8a37
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2021-07-05 22:12:00 +0200

    test scenario 2

--- src/main/java/mw/metrics/teams/TeamService.java
+++ src/main/java/mw/metrics/teams/TeamService.java
@@ -2,6 +2,10 @@ package mw.metrics.teams;
 
 import com.sun.management.UnixOperatingSystemMXBean;
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mw.metrics.teams.model.TeamCaptainDTO;
 import mw.metrics.teams.model.TeamCode;
@@ -21,7 +25,9 @@ public class TeamService {
 
     private long openFileDesc;
     private long maxFileDesc;
+    private Random rand=new Random();
 
+    private static List<Object> db=new ArrayList<>();
 
     public TeamService(WebClient webClient) {
         b= (UnixOperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
@@ -30,9 +36,11 @@ public class TeamService {
 
     public TeamScoreDTO score(TeamCode teamCode) {
         var uri = UriComponentsBuilder.fromUriString(detailsQueryString).build(teamCode.name());
-
+        db.add(new MyObject("Score", rand.nextInt()));
         var result = webClient.get().uri(uri).retrieve().bodyToMono(TeamDetailsDTO.class).block();
         log.info(String.format("Open files %s, Max files %s", b.getOpenFileDescriptorCount(),b.getMaxFileDescriptorCount()));
+
+
         return TeamScoreDTO.from(teamCode,result.getPosition());
     }
 
@@ -40,6 +48,14 @@ public class TeamService {
         var uri = UriComponentsBuilder.fromUriString(playersQueryString).build(teamCode.name());
         var result = webClient.get().uri(uri).retrieve().bodyToMono(TeamPlayersDTO.class).block();
         log.info(String.format("Open files %s, Max files %s", b.getOpenFileDescriptorCount(),b.getMaxFileDescriptorCount()));
+        db.add(new MyObject("Captain", rand.nextInt()));
         return TeamCaptainDTO.from(teamCode, result.getCaptain());
+    }
+
+    @AllArgsConstructor
+    class MyObject {
+
+        String name;
+        Integer account;
     }
 }
